5fd159c2ae8e88c770991c574d4842653297aedd
Jasondmcgee <jasondmcgee17@gmail.com>
2019-08-28 16:42:53 -0700

    made some progress

--- madlibs.py
+++ madlibs.py
@@ -1,16 +1,32 @@
 
 propernouns = []
+nouns = []
 adjectives = []
 verbs = []
 word = str()
 
 
 def propernoun(word):
-    word = raw_input("enter a proper noun: ")
+    word = input("enter a proper noun: ")
+    word = str(word)
     propernouns.append(word)
 
 
-propernoun(word)
-propernoun(word)
+def noun(word):
+    word = input("enter a noun: ")
+    word = str(word)
+    nouns.append(word)
 
-print("this morning I went to " + propernouns[0] + "'s" + " house with my friend " + propernouns[1] + ".")
+
+def verb(word):
+    word = input("enter a verb: ")
+    word = str(word)
+    verbs.append(word)
+
+
+propernoun(word)
+propernoun(word)
+noun(word)
+verb(word)
+
+print("this morning I went to " + propernouns[0] + "'s" + " house with my friend " + propernouns[1] + ". we played videogames on the couch until " + propernouns[1] + " mom, " + propernouns[0] + ", walked in with some " + nouns[0] + "s" + ". " + nouns[0] + " was so delecious that stopped" + verbs[0] + "ing")
