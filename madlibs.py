95b53941da95c92ef54194e18279e3cb4da89059
Jasondmcgee <jasondmcgee17@gmail.com>
2019-09-02 20:18:01 -0700

    finished:)

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,12 @@
 
 propernouns = []
 nouns = []
+pluralnouns = []
 adjectives = []
 verbs = []
+places = []
+numbers = []
+bodyparts = []
 word = str()
 
 
@@ -10,6 +14,12 @@ def propernoun(word):
     word = input("enter a proper noun: ")
     word = str(word)
     propernouns.append(word)
+
+
+def pluarlnoun(word):
+    word = input("enter a noun (pluarl): ")
+    word = str(word)
+    pluralnouns.append(word)
 
 
 def noun(word):
@@ -24,9 +34,42 @@ def verb(word):
     verbs.append(word)
 
 
-propernoun(word)
-propernoun(word)
+def place(word):
+    word = input("enter a place: ")
+    word = str(word)
+    places.append(word)
+
+
+def adjective(word):
+    word = input("enter a adjective: ")
+    word = str(word)
+    adjectives.append(word)
+
+
+def number(word):
+    word = input("enter a number: ")
+    word = str(word)
+    numbers.append(word)
+
+
+def bodypart(word):
+    word = input("enter a bodypart: ")
+    word = str(word)
+    bodyparts.append(word)
+
+
+pluarlnoun(word)
+place(word)
 noun(word)
+pluarlnoun(word)
+noun(word)
+adjective(word)
+verb(word)
+number(word)
+adjective(word)
+bodypart(word)
 verb(word)
 
-print("this morning I went to " + propernouns[0] + "'s" + " house with my friend " + propernouns[1] + ". we played videogames on the couch until " + propernouns[1] + " mom, " + propernouns[0] + ", walked in with some " + nouns[0] + "s" + ". " + nouns[0] + " was so delecious that stopped" + verbs[0] + "ing")
+#Romeo and Juliet: Prologue ad-Lib
+
+print("Two " + pluralnouns[0] + ", both alike in dignity, In fair " + places[0] + ", where we lay our scene, From ancient" + nouns[0] + " to new mutiny, Where civil blood makes civil hands unclean. From forth the fatal loins of these two foes A pair of star-cross`d " + pluralnouns[1] + " take their life; Whole misadventured piteous overthrows Do with their" + nouns[1] + " bury their parents` strife. The fearful passage of their" + adjectives[0] + "love, And the continuance of their parents` rage, Which, but their children`s end, nought could" + verbs[0] + ", Is now the " + numbers[0] + " hours` traffic of our stage; The which if you with " + adjectives[0] + bodyparts[0] + " attend, What here shall " + verbs[1] + ", our toil shall strive to mend")
